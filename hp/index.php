1b2b0796e0e664d110e271bbfb1f8781623cb09c
fotlan33 <christophe@fotlan.com>
2017-03-31 15:56:19 +0200

    Create index.php

---
+++ hp/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>FotLan</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="viewport" content="width=device-width" />
+	<link rel="stylesheet" type="text/css" href="/res/css/fotlan.css" />
+	<link rel="stylesheet" type="text/css" href="/res/css/hp.css" />
+	<script type="text/javascript" src="/res/js/fotlan.js"></script>
+</head>
+<body>
+	<div class="profile">&nbsp;</div>
+	<div><img src="/res/img/fotlan.png" alt="FotLan" width="140" height="50" /></div>
+	<div class="hp_ground">
+		<a class="hp_link" href="http://www.google.fr/"><img src="img/google.png" alt="Google" /></a>
+		<a class="hp_link" href="http://mail.fotlan.com/"><img src="img/gmail.png" alt="Fotlan GMail" /></a>
+		<a class="hp_link" href="http://home.fotlan.com/mp/"><img src="img/music.png" alt="Fotlan Player" /></a>
+		<a class="hp_link" href="http://home.fotlan.com/photos/"><img src="img/photos.jpg" alt="Phototh&egrave;que" /></a>
+		<a class="hp_link" href="http://home.fotlan.com/vtt/"><img src="img/vtt.jpg" alt="Traces VTT" /></a>
+		<a class="hp_link" href="http://home.fotlan.com/banque/"><img src="img/banque.png" alt="Comptes bancaires" /></a>
+		<a class="hp_link" href="http://home.fotlan.com/radios/"><img src="img/radios.jpg" alt="Radios" title="Radios" /></a>
+		<a class="hp_link" href="mediatheque.htm"><img src="img/library.jpg" alt="Médiathèque" /></a>
+		<a class="hp_link" href="http://home.fotlan.com/contacts/"><img src="img/contacts.png" alt="Carnet d'adresses" /></a>
+		<a class="hp_link" href="http://home.fotlan.com/corsica2010/"><img src="img/corsica.png" alt="Corsica 2010" /></a>
+		<a class="hp_link" href="http://www.lachainemeteo.com/meteo/courcouronnes/france/prevision_meteo_courcouronnes_france_ville_1394_0.php"><img src="img/meteo.png" alt="Meteo" /></a>
+		<a class="hp_link" href="https://www.axabanque.fr/connexion/index.html"><img src="img/axa.png" alt="AXA Banque" /></a>
+		<a class="hp_link" href="https://mabanque.bnpparibas/"><img src="img/bnp.png" alt="BNP Paribas" /></a>
+		<a class="hp_link" href="https://particuliers.secure.lcl.fr/index.html"><img src="img/lcl.png" alt="LCL" /></a>
+		<a class="hp_link" href="http://www.pagesjaunes.fr/"><img src="img/pagesjaunes.png" alt="Pages jaunes" /></a>
+		<a class="hp_link" href="http://my.ebay.fr/ws/eBayISAPI.dll?MyeBay"><img src="img/ebay.png" alt="eBay" /></a>
+		<a class="hp_link" href="http://www.allocine.fr/seance/salle_gen_csalle=B0059.html"><img src="img/cgr.png" alt="Cin&eacute;ma Evry" /></a>
+		<a class="hp_link" href="/ceb/"><img src="img/ceb.png" alt="Le compte est bon" title="Le compte est bon" /></a>
+		<a class="hp_link" href="/plan/"><img src="img/plan.png" alt="Plan d'acc&egrave;s" /></a>
+		<a class="hp_link" href="/cv/"><img src="img/cv.png" alt="CV" /></a>
+	</div>
+</body>
+</html>
