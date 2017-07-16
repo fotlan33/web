85a5557518beb14d83b63618755755729205f96e
Christophe <christophe@fotlan.com>
2017-07-16 11:17:48 +0200

    Test AWS

--- photos/form.php
+++ photos/form.php
@@ -6,16 +6,31 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 </head>
 <body>
-	<input type="file" id="fileinput" multiple="multiple" accept="image/*" />
+	<input type="file" id="uploadfiles" multiple="multiple" accept="image/*" />
 	<script type="text/javascript" src="/res/js/jquery-3.2.0.min.js"></script>
 	<script type="text/javascript">
 	var uploadfiles = document.querySelector('#uploadfiles');
 	uploadfiles.addEventListener('change', function () {
 	    var files = this.files;
-	    for(var i=0; i<files.length; i++){
+	    for(var i=0; i<files.length; i++) {
 	        uploadFile(this.files[i]); // call the function to upload the file
 	    }
 	}, false);
+	
+	function uploadFile(file){
+	    var url = 'server/index.php';
+	    var xhr = new XMLHttpRequest();
+	    var fd = new FormData();
+	    xhr.open("POST", url, true);
+	    xhr.onreadystatechange = function() {
+	        if (xhr.readyState == 4 && xhr.status == 200) {
+	            // Every thing ok, file uploaded
+	            console.log(xhr.responseText); // handle response.
+	        }
+	    };
+	    fd.append("upload_file", file);
+	    xhr.send(fd);
+	}
 	</script>
 </body>
 </html>
